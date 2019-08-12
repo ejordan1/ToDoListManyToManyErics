ca475eb445791e035d546ba8493399405ab976cc
Emerson Jordan and Erik Irgens <emerson@>
2019-08-12 10:35:29 -0700

    Add functionality for many-to-many relationships, using migrations with entity.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -1,19 +1,16 @@
 using System.Collections.Generic;
 
-
 namespace ToDoList.Models
 {
-    public class Category
+  public class Category
     {
+        public Category()
+        {
+            this.Items = new HashSet<CategoryItem>();
+        }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Item> Items { get; set; }
-        public Category()
-        {
-            this.Items = new HashSet<Item>();
-        }
-
-
+        public virtual ICollection<CategoryItem> Items { get; set; }
     }
 }
